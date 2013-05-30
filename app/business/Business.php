5bba7761be586885b67a7481e6df54c5da827bbc
Pedro Thiago <Pedro Thiago@192.168.1.5>
2013-05-30 19:29:33 -0300

    teste

---
+++ app/business/Business.php
@@ -0,0 +1,10 @@
+<?php 
+
+abstract class Business{
+	abstract public function salva();
+	abstract public function altera();
+	abstract public function consulta();
+	abstract public function exclui();
+	
+}
+?>

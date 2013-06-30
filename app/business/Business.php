6c55d945d4574be17452cd09bd548e1013fe366b
Allan Pereira <allanpereira@hotmail.com.br>
2013-06-30 14:26:02 -0300

    Login - Quase funcionando

--- app/business/Business.php
+++ app/business/Business.php
@@ -5,9 +5,8 @@ abstract class Business{
 	protected $dao;
 
 	abstract public function salva($objeto);
-	abstract public function altera();
-	abstract public function buscaProjeto($idProjeto);
-	abstract public function exclui();
+	abstract public function altera($id);
+	abstract public function exclui($id);
 }
 
 ?>

8cf6e0d7947492f69cb2e9da01eecc070500d681
Marcos Ramos <ms.ramos@outlook.com>
2013-07-04 17:05:00 -0300

    Verificação de arquivo de teste

---
+++ app/test/AnyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class AlgumaCoisaTest extends PHPUnit_Framework_TestCase {
+
+	public function setUp() {
+			
+	}
+	
+	public function testAlgumaCoisa() {
+		$this->assertEquals(TRUE, TRUE);
+	}
+	
+	public function testOutraCoisa() {
+		$this->assertEquals(TRUE, FALSE);		
+	}	
+}
+
+?>

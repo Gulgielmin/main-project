d1dbb18f049220b2c24405f8c588ea5541a7c3d6
unknown <wilker-mc@hotmail.com>
2013-07-04 15:52:52 -0300

    Mudando public para private em usuraio.

--- app/test/account.doTest.php
+++ app/test/account.doTest.php
@@ -1,26 +1,26 @@
 <?php
-include "../app/controller/LoginController.php";
-class LoginControllerTest extends PHPUnit_Framework_TestCase{
+class accountdoTest extends PHPUnit_Framework_TestCase{
 	
 	
-	public function TestPreenchimento()
+	/*public function ()
 	{
-		$obj = new LoginController();
 		$this->assertEquals (true,$obj->verficaCamposLogin($obj->email,$obj->senha));
 		$this->assertEquals (false,$obj->verficaCamposLogin('',$obj->senha));
 		$this->assertEquals (false,$obj->verficaCamposLogin($obj->email,''));	
-	}
+	}*/
 	
 	public function TestefetuarLogin()
 	{
-		$obj = new LoginController();
-		$this->assertEquals (true,$obj->efetuarLogin($email,$senha));
+		include "../../account/account.do.php?action=login";
+		$_POST['email']="";
+		$_POST['senha']="";
+		$this->assertEquals (false,$obj->efetuarLogin($_POST));
 	} 
 	
-	public function redirecionarLogin()
+	/*public function redirecionarLogin()
 	{
 		$obj = new LoginController();
 		$this->assertEquals ('projeto/index.php',$obj-> redirecionarLogin($usuario));
-	}
+	}*/
 }
 

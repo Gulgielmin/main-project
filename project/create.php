7c3b2108214c983ee923e7eb219209a8c4be660c
Allan Pereira <allanpereira@hotmail.com.br>
2013-07-14 14:04:53 -0300

    Estilizacao Interface Cadastro Projeto

--- project/create.php
+++ project/create.php
@@ -22,56 +22,45 @@
 
 		<?php require '../shared/navigation_bar.php';?>
 
-		<div class="content">
-        
-
-			<form method="post" action="handlerProjeto.php?action=2"
-				id="formCadastroProjeto">
-				<div class="labels">
-					<label for="nomePojeto">Nome:</label>
-					<label for="dataInicio">In&iacute;cio:</label>
-					<label for="dataTermino">Fim:</label>
-					<label for="dadosCusto">DADOS DE CUSTOS</label><br />
-					<label for="previsaoCusto">Or&ccedil;amento:</label>
-					<label for="periodiciadade"></label>
-					<label for="quantidadePeriodos">Quantidade de Períodos:</label>
-					 <label for="dataDesconto">Data do Primeiro Desconto de Valor:</label>
-				
-				
-				</div>
-				
-				<div class="inputs">
-				
-					<input type="text" name="nomeProjeto" /> 
-					<input type="text" name="dataInicio" id="from" /> 
-					<input type="text" name="dataTermino" id="to" />  
-					<input type="text" name="previsaoCusto" /> 
-					<select name="periodicidade">
-						<option value="1">Única</option>
-						<option value="2">Diária</option>
-						<option value="3">Semanal</option>
-						<option value="4">Quinzenal</option>
-						<option value="5">Mensal</option>
-						<option value="6">Semestral</option>
-						<option value="7">Anual</option>
-					</select> 
-					<input type="text" name="quantidadePeriodos" value="" /> 
-					<input type="text" name="dataDesconto" class="datepicker" />
-					<input type="submit" value="Enviar" /> 
-					<input type="button" class="button" value="Cancelar" id="botaoCancelar" />
-				
-				</div>
-				
-				</form>
-
-			<div id="sidebar">
+		<div class="content">		
+		
+			<div id="sidebar" class="box">
 				<div>
-					<h2 class="title">Opções</h2>
+					<div id="title-sidebar" class="destak">
+						Opções
+					</div>
 					<ul id="options-list">
-						<li><a href="index.php">Ir para Meus Projetos</a></li>
+						<li><a href="index.php">Meus Projetos</a></li>
 					</ul>
 				</div>
 			</div>
+        
+			<div class="inputs box">
+
+			<form method="post" action="handlerProjeto.php?action=2"
+				id="formCadastroProjeto">
+				<fieldset>
+					<legend class="destak">Cadastro de Projeto</legend>
+				
+					<label for="nomePojeto">Nome:</label>
+						<input type="text" name="nomeProjeto" /> <br />
+					
+					<label for="dataInicio">In&iacute;cio:</label>
+						<input type="text" name="dataInicio" id="from" /> <br /> 
+					
+					<label for="dataTermino">Fim:</label>
+						<input type="text" name="dataTermino" id="to" />  <br />
+					
+					<label for="orcamento">Or&ccedil;amento:</label> 
+						<input type="text" name="orcamento" />  <br />
+					
+					<input type="submit" value="Enviar" class="button" /> 
+					<input type="button" class="button" value="Cancelar" id="botaoCancelar" />
+				
+				</fieldset>				
+				</form>
+				
+				</div>
 		</div>
 		<?php require '../shared/footer.php';?>
 	</div>
